a09c24a296c1bd0825cb6f719c3e585090be37cd
Kásio Eduardo <kasioeduardo13@gmail.com>
2019-11-30 21:37:20 -0300

    FIX: new backend with AdonisJS

--- gympoint/start/app.js
+++ gympoint/start/app.js
@@ -17,7 +17,8 @@ const providers = [
   '@adonisjs/cors/providers/CorsProvider',
   '@adonisjs/lucid/providers/LucidProvider',
   '@adonisjs/validator/providers/ValidatorProvider',
-  '@adonisjs/antl/providers/AntlProvider'
+  '@adonisjs/antl/providers/AntlProvider',
+  'adonis-acl/providers/AclProvider'
 ];
 
 /*
@@ -29,7 +30,10 @@ const providers = [
 | Providers for migrations, tests etc.
 |
 */
-const aceProviders = ['@adonisjs/lucid/providers/MigrationsProvider'];
+const aceProviders = [
+  '@adonisjs/lucid/providers/MigrationsProvider',
+  'adonis-acl/providers/CommandsProvider'
+];
 
 /*
 |--------------------------------------------------------------------------
@@ -43,7 +47,10 @@ const aceProviders = ['@adonisjs/lucid/providers/MigrationsProvider'];
 |   { Route: 'Adonis/Src/Route' }
 |
 */
-const aliases = {};
+const aliases = {
+  Role: 'Adonis/Acl/Role',
+  Permission: 'Adonis/Acl/Permission'
+};
 
 /*
 |--------------------------------------------------------------------------
