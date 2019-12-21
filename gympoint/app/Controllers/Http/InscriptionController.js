d4c40ba12a5bf3bb18f63769bd11b483db58e7fc
Kásio Eduardo <kasioeduardo13@gmail.com>
2019-12-21 12:18:10 -0300

    chore: verify if student have already inscription

--- gympoint/app/Controllers/Http/InscriptionController.js
+++ gympoint/app/Controllers/Http/InscriptionController.js
@@ -3,7 +3,7 @@
 const Kue = use('Kue');
 const Job = use('App/Jobs/NewInscriptionMail');
 
-const { addMonths, parseISO } = require('date-fns');
+const { addMonths, parseISO, isAfter } = require('date-fns');
 
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Inscription = use('App/Models/Inscription');
@@ -12,23 +12,33 @@ const Plan = use('App/Models/Plan');
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Student = use('App/Models/Student');
 
+const Database = use('Database');
+
 class InscriptionController {
   async index() {
     const inscriptions = await Inscription.all();
     return inscriptions;
   }
 
-  async store({ request }) {
+  async store({ request, response }) {
     const { student_id, start_date, plan_id } = request.only([
       'student_id',
       'plan_id',
       'start_date'
     ]);
 
+    const [hasInscription] = await Database.table('inscriptions')
+      .where('student_id', student_id)
+      .orderBy('created_at', 'desc')
+      .limit(1);
+
+    if (hasInscription && isAfter(hasInscription.end_date, new Date()))
+      return response
+        .status(401)
+        .send({ message: 'This student already has an active inscription' });
+
     const plan = await Plan.findOrFail(plan_id);
-
     const student = await Student.findOrFail(student_id);
-
     const end_date = addMonths(parseISO(start_date), plan.duration);
 
     const inscription = await Inscription.create({
